df0ef4832131913712bb1269a99f769f09e2c313
Matin <matinimam77@gmail.com>
2022-04-29 15:52:14 +0530

    Base Project folder
    First Unity Initialization commit

---
+++ Assets/Scripts/Hand.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Hand 
+{
+    public Card[] cards = new Card[3];
+    public Transform[] positions = new Transform[3];
+    public string[] animNames = new string[3];
+    public bool isPlayer;
+
+    public void RemoveCard(Card card)
+    {
+        for(int i = 0; i < 3; i++)
+        {
+            if(cards[i] == card)
+            {
+                GameObject.Destroy(cards[i].gameObject);
+                cards[i] = null;
+                if (isPlayer)
+                    GameController.instance.playerDeck.DealCard(this);
+                else
+                    GameController.instance.enemyDeck.DealCard(this);
+                break;
+            }
+        }
+    }
+    internal void ClearHand()
+    {
+        for(int i = 0; i < 3; i++)
+        {
+            GameObject.Destroy(cards[i].gameObject);
+            cards[i] = null ;
+        }
+    }
+}
